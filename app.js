f98287bf0abede186bb6f3bea1135ec834bff587
John Kim <johnkimmsterr@gmail.com>
2021-06-09 12:37:48 -0400

    connected to mailchimp api, environment variables hidden

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+require('dotenv').config();
 
 const app = express();
 
-app.listen(3001, ()=> {
-    console.log("server listening at port: 3001");
-})
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: { FNAME: firstName, LNAME: lastName },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us6.api.mailchimp.com/3.0/lists/" + process.env.LIST_ID;
+
+  const options = {
+    method: "POST",
+    auth: "mother:" + process.env.API_KEY,
+  };
+
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.listen(3001, () => {
+  console.log("server listening at http://localhost:3001");
+});
+
