6b80a9f8672a569b928d9fd5fc906006b97fd7a7
John Kim <johnkimmsterr@gmail.com>
2021-06-09 12:53:34 -0400

    success and failure pages

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.post("/", (req, res) => {
   };
 
   const request = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+    } else {
+        res.sendFile(__dirname + "/failure.html")
+    }
+
     response.on("data", (data) => {
       console.log(JSON.parse(data));
     });
