c1a9a3d878fa54557238eb878f72df8fc83b559e
John Kim <johnkimmsterr@gmail.com>
2021-06-09 13:33:17 -0400

    added Procfile, deploying to heroku

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("server listening at http://localhost:3001");
 });
